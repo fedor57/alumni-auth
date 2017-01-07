3fb22f397b369896c435228cfab0eadab7875802
Fedor Romanenko <fedor57@gmail.com>
2017-01-08 02:21:06 +0300

    Formatting improved - ability to close about jumbotron - saving status in cookie - buttons formatting improved - better invite edit box formatting

--- app/static/app/scripts/appl.js
+++ app/static/app/scripts/appl.js
@@ -7,3 +7,34 @@ $(function () {
         }
     });
 });
+
+function getIntCookie(name, def)
+{
+    var val = Cookies.get(name);
+    if (about === undefined)
+        return def;
+    val = parseInt(val, 10);
+    if (isNaN(val))
+        return def;
+    return val;
+}
+
+$(document).ready(function () {
+    var about = getIntCookie('about', 1);
+
+    if (about < 1)
+    {
+        $("#about").addClass('hidden');
+        $("#what-is-this").removeClass('hidden');
+    }
+    $("#about").click(function (e) {
+        $(this).addClass('hidden');
+        $("#what-is-this").removeClass('hidden');
+        Cookies.set('about', 0);
+    });
+    $("#what-is-this").click(function (e) {
+        $(this).addClass('hidden');
+        $("#about").removeClass('hidden');
+        Cookies.set('about', 1);
+    });
+});
