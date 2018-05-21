30ec1ebd1d102340cee7fad5d938203d6a86b2ec
Nikolay Markov <nikolai.markov@icloud.com>
2018-05-21 23:08:00 +0000

    Add file argument to load_alumni command

--- app/management/commands/load_alumni.py
+++ app/management/commands/load_alumni.py
@@ -7,12 +7,16 @@ from app.models import alumni as Alumnus
 class Command(BaseCommand):
     help = 'Loads alumni from data/alumni-db.tsv'
 
+    def add_arguments(self, parser):
+        parser.add_argument('file_tsv', default=settings.BASE_DIR + '/data/alumni-db.tsv')
+
     def handle(self, *args, **kwargs):
-        with open(settings.BASE_DIR + '/data/alumni-db.tsv') as f:
+        with open(kwargs['file_tsv']) as f:
             for line in f:
                 full_name, year, letter = line.strip().split('\t')
-                a = Alumnus(full_name=full_name, year=int(year), letter=letter)
-                a.added_by = 'import'
-                a.save()
+                a, created = Alumnus.objects.get_or_create(full_name=full_name, year=int(year), letter=letter)
+                if created:
+                    a.added_by = 'import'
+                    a.save()
 
 
